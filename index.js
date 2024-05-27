0437cebe1c6d6679c4d55379131a902d8b36353b
fame372 <70764830+fame372@users.noreply.github.com>
2024-05-27 10:06:45 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+
+
+
+let numberEl = document.getElementById("number-el")
+let convertBtn = document.getElementById("convert-btn")
+let meterEL = document.getElementById("meter-el")
+
+convertBtn.addEventListener("click", function() {
+   
+})
+
+
+  
